3f56b3e0188a4571c617ef22052d0f40f12d6056
SirAlyon <alessiosada04@gmail.com>
2022-04-16 15:11:58 +0200

    add bonus 1 and bonus 2

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const posts = [
     {
         id: 1,
         name: 'Alessio',
+        surname: 'Sada',
         user_img: 'https://picsum.photos/200',
         date: '04/12/2022',
         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sapiente veniam vero unde cumque pariatur natus laboriosam, cupiditate nobis voluptate tempora. Atque repellat debitis unde',
@@ -21,6 +22,7 @@ const posts = [
     {
         id: 2,
         name: 'Alessandro',
+        surname: 'Rossi',
         user_img: 'https://picsum.photos/200',
         date: '04/12/2022',
         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sapiente veniam vero unde cumque pariatur natus laboriosam, cupiditate nobis voluptate tempora. Atque repellat debitis unde',
@@ -30,10 +32,21 @@ const posts = [
     {
         id: 3,
         name: 'Martina',
+        surname: 'Bianchi',
         user_img: 'https://picsum.photos/200',
         date: '03/15/2022',
         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sapiente veniam vero unde cumque pariatur natus laboriosam, cupiditate nobis voluptate tempora. Atque repellat debitis unde',
-        image: 'https://picsum.photos/480/250',
+        image: '',
+        likes: 150
+    },
+    {
+        id: 4,
+        name: 'Fabio',
+        surname: 'Bianchi',
+        user_img: 'https://picsum.photos/200',
+        date: '03/15/2022',
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sapiente veniam vero unde cumque pariatur natus laboriosam, cupiditate nobis voluptate tempora. Atque repellat debitis unde',
+        image: '',
         likes: 150
     }
 ]
@@ -45,11 +58,9 @@ const likedPosts = []
 
 const postsElement = document.getElementById('posts')
 
-posts.forEach((element, i) =>{
+posts.forEach((element) =>{
     generatedPosts(postsElement, element)
 })
-
-
 
 const likes = document.querySelectorAll('.likes');
 likes.forEach((like) => {
@@ -74,30 +85,14 @@ likes.forEach((like) => {
 
         console.log(likedPosts);
         
-        /* console.log(this.id);
-        const postId = this.id.replace("like-button-", "")
-        console.log(postId);
-
-        const postElement = document.getElementById("post-" + postId);
-        console.log(postElement)
-
-        const counterElement = postElement.querySelector(".counter");
-        console.log(counterElement);
-
-        counterElement += 1 */
-        // posts.likes += 1
-        // postsElement.innerHTML = ''
-        // posts.forEach((element, i) =>{
-        //     generatedPosts(postsElement, element)
-        // })
-        
     })
 })
 
 
 
 function generatedPosts(father_element, object_name) {
-    
+
+
     father_element.insertAdjacentHTML('beforeend', `
     <div class="post bg-white p-2 mt-5 mb-3" id="post-${object_name.id}">
         <div class="header d-flex align-items-center">
@@ -105,14 +100,14 @@ function generatedPosts(father_element, object_name) {
                 <img src="${object_name.user_img}" alt="">
             </div>
             <div class="d-flex flex-column mx-3">
-                <span>${object_name.name}</span>
-                <span>${object_name.date}</span>
+                <span>${object_name.name + " " + object_name.surname}</span>
+                <span class="date">${object_name.date}</span>
             </div>
         </div>
         <div class="content mt-2">
             <p>${object_name.text}</p>
             <div class="post_image">
-                <img class="w-100" src="${object_name.image}" alt="">
+                <img class="w-100" id="post_image" src="${object_name.image}" alt="">
             </div>
         </div>
         <div class="d-flex justify-content-around p-3">
@@ -120,4 +115,30 @@ function generatedPosts(father_element, object_name) {
             <span>Piace a <strong class="counter">${object_name.likes}</strong> persone</span>
         </div>
     </div>`)
+
+
+    if (object_name.image === ""){
+        object_name.image = object_name.name.charAt(0) + object_name.surname.charAt(0)
+        //console.log(object_name.image);
+        const postNumber = document.getElementById("post-" + object_name.id);
+        //console.log(postNumber);
+        const postImageElement = postNumber.querySelector('.post_image')
+        //console.log(postImageElement);
+        postImageElement.innerHTML = `<span class="no_image">${object_name.image}</span>`
+    }
+
+    //Cambio il formato della data da americano ad italiano
+    const postNumber = document.getElementById("post-" + object_name.id);
+    const postDate = postNumber.querySelector('.date').innerHTML
+    console.log(postDate);
+    const splitDate = postDate.split("/")
+    console.log(splitDate, splitDate[1]);
+    const finalDate = `${splitDate[1]}/${splitDate[0]}/${splitDate[2]}`
+    console.log(finalDate);
+    console.log(postNumber);
+    const postDateElement = postNumber.querySelector('.date')
+    console.log(postDateElement);
+    postDateElement.innerHTML = finalDate
+    
+    
 }
